02bcf337f24177a7bb3600aa93ce701473ad6c26
Brandon Thomas <brandon.thomas@dxydoes.com>
2014-03-28 23:54:42 -0400

    geolocation stuffs

--- Beeline/GTMainViewController.m
+++ Beeline/GTMainViewController.m
@@ -27,9 +27,31 @@
 
 - (IBAction)sendLocation:(id)sender
 {
+    float latitude, longitude;
+    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+    CLLocation *here = locationManager.location;
+    
+    // Get the accuracy of the location
+    float acc = here.horizontalAccuracy;
+    
+    // Call the WS to check-in
+    /*
+    NSDictionary* postParams = @{
+                                 @"settingsID": [NSNumber numberWithInt:self.appSettings.settingsID],
+                                 @"verified": [NSNumber numberWithInt:((self.checkinVerified) ? 1 : 0)],
+                                 @"accuracy": [NSNumber numberWithFloat:acc],
+                                 @"latitude": [NSNumber numberWithFloat:here.coordinate.latitude ],
+                                 @"longitude": [NSNumber numberWithFloat:here.coordinate.longitude ],
+                                 @"timestamp": [NSString stringWithFormat:@"2013-10-16 12:00:00"]
+                                 };
+     */
+
+    latitude = here.coordinate.latitude;
+    longitude = here.coordinate.longitude;
+    
     PFPush *push = [[PFPush alloc] init];
     [push setChannel:@"Location"];
-    [push setMessage:@"Crap butt."];
+    [push setMessage:[NSString stringWithFormat:@"My Location: %f, %f", latitude, longitude]];
     [push sendPushInBackground];
 }
 
